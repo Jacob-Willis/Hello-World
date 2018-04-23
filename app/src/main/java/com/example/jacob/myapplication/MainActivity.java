0814c5c212761f327b9578237f184ff3b005a542
Jacob Willis <37282838+Jacob-Willis@users.noreply.github.com>
2018-04-24 00:26:50 +1000

    Hey
    Hey

--- app/src/main/java/com/example/jacob/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jacob/myapplication/MainActivity.java
@@ -57,6 +57,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void helloWorld(View view) {
         helloTxt = (TextView) findViewById((R.id.helloTxt));
-        helloTxt.setText("Hello World!");
+        helloTxt.setText("Hey!");
     }
 }
